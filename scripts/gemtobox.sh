e98c9d23fe59aac7e7414cec30a3a6cff8e86149
Steve Meadows <smeadows@sonichealthcareusa.com>
2021-04-06 19:32:20 -0500

    Add upload directory

--- scripts/gemtobox.sh
+++ scripts/gemtobox.sh
@@ -3,14 +3,14 @@
 gemfile=$1
 host=$2
 wdir=$(pwd)
-out=./scripts/upload.sh
+dir=$(dirname $gemfile)
+verfile=$dir/.ruby-version
+out=./scripts/upload/$(basename $dir).sh
 
 if [ $(basename $gemfile) != "Gemfile" ]; then
   echo "Not a Gemfile" && exit
 fi
 
-dir=$(dirname $gemfile)
-verfile=$dir/.ruby-version
 if [ ! -f $verfile ]; then
   echo "No ruby version" && exit
 fi
@@ -21,7 +21,13 @@ cache=$(rvm $version do rvm gemdir)/cache
 cd $dir
 gems=$(rvm $version do bundle | sed -e '/Using/!d' -e '/from/d' -e 's/Using \(.*\) \([0-9].*[0-9]\)/\1-\2/' | sort)
 cd $wdir
+
+echo "Creating upload: $out"
 (for f in $gems; do
-  echo gem inabox $cache/${f}*.gem -g $host
+  if [ -f $cache/${f}.gem ]; then
+    echo gem inabox $cache/${f}.gem -g $host
+  else
+    echo gem inabox $cache/${f}*.gem -g $host
+  fi
 done) > $out
 chmod +x $out
